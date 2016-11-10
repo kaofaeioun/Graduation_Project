ebae36c2902c0aa7ebbf0dd67439da781d89e1e0
shaohonglee <d38372002@gmail.com>
2016-11-10 15:02:56 +0800

    燈燈燈燈
    登入

---
+++ PHP/Project/views/login.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<?php
+include('mysql_connect.php');
+?>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<link href="http://fonts.googleapis.com/css?family=Cabin:400,500,600,bold" rel="stylesheet" type="text/css" >
+	<link href="http://fonts.googleapis.com/css?family=PT+Sans+Narrow:regular,bold" rel="stylesheet" type="text/css" >
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+	<link rel="stylesheet" href="CSS/login.css">
+	<link rel="stylesheet" href="CSS/all.css">
+	<title>MicMusic</title>
+</head>
+<body>
+	<div class="wrap">
+		<div class="header">
+			<h1><div class="Logo"><img src="image/Logo2.png"></div></h1>
+			<div class="menu">
+				<div class="search">
+					<input type="text" class="search_blank" placeholder="輸入id找歌手">
+					<input type="image" class="search_image" src="image/search.png">
+				</div>
+				<br>
+				<ul>
+					<li><a href="battle.html"><img src="image/menu_battle.png" width="15%">  &nbsp<b>大亂鬥</b></a></li>
+					<li><a href="channel.html"><img src="image/menu_personal.png" width="15%"> &nbsp<b>個人頻道</b></a></li>
+					<li><a href="personalinfo.php"><img src="image/menu_person_info.png" width="15%"> &nbsp<b>我的資料</b></a></li>
+					<li><a href="setting.html"><img src="image/menu_setting.png" width="15%"> &nbsp<b>設定</b></a></li>
+				</ul>
+			</div>
+
+		</div>
+	</div><hr>
+	<div class="wrap">
+		<div id="content">
+			<h2>登入<b>/</b><br>Login</h2>
+				<div class="profile_pic"><img src="image/profilepic.jpg" alt=""></div>
+				<div class="profile">
+				
+					<ul>
+					<form method="post" action="">
+						<li>
+							<b>請輸入用戶/ID</b>
+							<div class="name_blank"><input type="text" name="id" required=""></div>
+						</li>
+						<li>
+							<b>請輸入密碼/Password</b>
+							<div class="name_blank"><input type="password" name="pwd" required=""></div>
+						</li>
+						<div class="clear"></div>	
+						
+
+						<div class="clear"></div>
+						<button type="submit" class="ctrl-standard typ-subhed fx-sliderIn" id ="sub">Submit</button>
+					</form>
+					</ul>
+					
+					<?php
+					header("Content-Type: text/html; charset=utf-8");
+  					if(isset($_POST['id'])&& isset($_POST['pwd'])){
+		        		$account = $_POST['id'];
+		    			$passwd = $_POST['pwd'];
+    					//搜尋資料庫資料
+		    			$sql = "SELECT * FROM User where User_ID =$account";
+		    			$result = mysqli_query($link,$sql);
+					 	if($row=mysqli_fetch_assoc($result)){
+					 		if($row['User_PWD']==$passwd){
+	        					setcookie('account',$account,time()+3600);
+	        					echo '<meta http-equiv=REFRESH CONTENT=0;url=battle.html>';
+	      					}else if($row['User_PWD']!=$passwd){
+	        					echo "<script>alert('密碼錯誤！')</script>";
+	      					}
+	      				}	
+	    				else{
+	    					echo "<script>alert('無此帳號！')</script>";
+  							}
+  						
+  					}	
+  					?>
+				</div>
+			<div class="clear"></div>
+		</div>	
+	</div>
+
+	<div class="footer_space">
+	<footer>
+		<h3>MicMusic</h3>
+		<p>© 2016 All rights reserved.</p>
+		<p>NUKIM 106專題開發</p>
+		<ul>
+			<li><a href="battle.html"><img src="image/menu_battle.png"></a></li>
+			<li><a href="channel.html"><img src="image/personal.png"></a></li>
+			<li><a href="personalinfo.php"><img src="image/person_info_chosen.png"></a></li>
+			<li><a href="setting.html"><img src="image/setting.png"></a></li>
+		</ul>
+	</footer>
+	</div>
+</body>
+</html>
--- PHP/Project/views/mysql_connect.php
+++ PHP/Project/views/mysql_connect.php
@@ -3,30 +3,13 @@ $db_server = "140.127.220.144";
 $db_name = "MICMUSIC";
 $db_user = "kaofaeioun";
 $db_passwd = "qwerqwer";
+$link = mysqli_connect($db_server, $db_user, $db_passwd,$db_name);
+if(mysqli_connect_errno($link)){
+	die("Can not connect DB");
+}
+        
 
-if(!mysql_connect($db_server, $db_user, $db_passwd,$db_name))
-        die("無法對資料庫連線");
+if(!mysqli_select_db($link, $db_name))
+        die("Can not use DB");
 
-mysql_query("SET NAMES utf8");
-
-if(!mysql_select_db($db_name))
-        die("無法使用資料庫");
-/*$servername = "140.127.220.144";
-$username = "kaofaeioun";
-$password = "qwerqwer";
-$dbnamne = "MICMUSIC";
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-mysql_select_db($dbname,$conn);
-// Check connection
-if(!mysql_ping ($conn)){
-  if($conn->connect_error){
-      die("連線失敗！！Connection failed: " . $conn->connect_error);
-  }else{
-      echo "連線成功！ Connected successfully";
-  }
-  $conn->close();
-}else{
-  echo "無法連線....";
-}*/
 ?>
