e4b2f3d8865ac37a20733423898835c4263804e9
Iamjenius <amily_22036812@yahoo.com.tw>
2016-11-24 00:34:17 +0800

    可以投票啦啦啦~

--- PHP/Project/views/battle_channel_1.php
+++ PHP/Project/views/battle_channel_1.php
@@ -34,13 +34,7 @@
 	<script type="text/javascript" src="./js/voip.js"></script>
 	<title>MicMusic</title>
 </head>
-<style>
-svg {
-	width: 300px;
-	height: 300px;
-	display: block;
-}
-</style>
+
 <body>
 	<script src="http://d3js.org/d3.v3.min.js"></script>
 	<script type="text/javascript">
@@ -112,90 +106,88 @@ svg {
 				<div class="vote">
 					<div class="board">
 						<div class="circle_area">
-								<div class="circle_1" id="circle"></div>
-								<div class="circle_2" id="CountDown"></div>
-								<div class="vote_like" id="like"></div>
-								<div class="vote_dislike" id="dislike"></div>
-								<!--測試用按鈕-->
-								<button class="like">Like</button>
-								<button class="dislike">Dislike</button>
-								<!--不用刪-->
+							<div class="circle_1" id="circle"></div>
+							<div class="circle_2" id="CountDown"></div>
+							<div class="vote_like" id="like"></div>
+							<div class="vote_dislike" id="dislike"></div>
+
+							<script>
+								var like_vote = 0,
+								    dislike_vote = 0,
+								    total_vote,like_R,dislike_R;
+
+								function CountLikeVote() {
+								  return like_R = like_vote / total_vote;
+								  console.log(like_R);
+								};
+								function CountDislikeVote() {
+								  return dislike_R = dislike_vote / total_vote;
+								  console.log(dislike_R);
+								};
+								/* Body插入svg g標籤*/
+								var svg = d3.select("body .board")
+									.append("svg")
+									.append("g");
+								/*設定長寬、圓半徑*/
+								var width = 200,
+								    height = 200,
+								    radius = 100;
+								/*D3內建layout Pie套件，並輸入資料。*/
+								var pie = d3.layout.pie()
+									.sort(null)
+									.value(function(d) {return d.value;});
+								/*D3內建svg Arc套件，並輸入內外圓半徑*/
+								var arc = d3.svg.arc()
+									.outerRadius(radius * 1)
+								  .innerRadius(radius * 0.75);
+
+								svg.append("g")
+									.attr("class", "fans");
+								svg.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+								var key = function(d){ return d.data.label; };
+
+								d3.select("#like").on("click", function(){
+								    like_vote = like_vote + 1;
+								    Votes(CountLikeVote());
+								});
+								d3.select("#dislike").on("click", function(){
+								    dislike_vote = dislike_vote + 1;
+								    Votes(CountDislikeVote());
+								});
+
+								function Votes(data) {
+								  total_vote = like_vote + dislike_vote;
+								  var dataset = [
+								  			{label:"Dislike", value:CountDislikeVote(), color:"#ED1B2E"},
+								        {label:"Like", value:CountLikeVote(), color:"#376FFF"}
+								  		];
+								  var fan = svg.select(".fans").selectAll("path.fan")
+								    .data(pie(dataset));
+
+								  fan.enter()
+								    .insert("path")
+								    .style("fill", function (d) {return d.data.color;})
+								    .attr("class", "fan");
+
+								  fan.transition().duration(1000)
+								  	.attrTween("d", function(d) {
+								  		this._current = this._current || d;
+								  		var interpolate = d3.interpolate(this._current, d);
+								  		this._current = interpolate(10);
+								  		return function(t) {
+								      	return arc(interpolate(t));
+								  		};
+								  	})
+
+								  	fan.exit()
+								  	.remove();
+								}
+							</script>
 						</div>
 					</div>
 					<!--投票開始-->
-					<script>
-						var like_vote = 0,
-						    dislike_vote = 0,
-						    total_vote,like_R,dislike_R;
-
-						function CountLikeVote() {
-						  return like_R = like_vote / total_vote;
-						  console.log(like_R);
-						};
-						function CountDislikeVote() {
-						  return dislike_R = dislike_vote / total_vote;
-						  console.log(dislike_R);
-						};
-						/* Body插入svg g標籤*/
-						var svg = d3.select("body .board")
-							.append("svg")
-							.append("g");
-						/*設定長寬、圓半徑*/
-						var width = 200,
-						    height = 200,
-						    radius = 100;
-						/*D3內建layout Pie套件，並輸入資料。*/
-						var pie = d3.layout.pie()
-							.sort(null)
-							.value(function(d) {return d.value;});
-						/*D3內建svg Arc套件，並輸入內外圓半徑*/
-						var arc = d3.svg.arc()
-							.outerRadius(radius * 1)
-						  .innerRadius(radius * 0.75);
-
-						svg.append("g")
-							.attr("class", "fans");
-						svg.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
-
-						var key = function(d){ return d.data.label; };
-
-						d3.select("#like").on("click", function(){
-						    like_vote = like_vote + 1;
-						    Votes(CountLikeVote());
-						});
-						d3.select("#dislike").on("click", function(){
-						    dislike_vote = dislike_vote + 1;
-						    Votes(CountDislikeVote());
-						});
-
-						function Votes(data) {
-						  total_vote = like_vote + dislike_vote;
-						  var dataset = [
-						  			{label:"Dislike", value:CountDislikeVote(), color:"#ED1B2E"},
-						        {label:"Like", value:CountLikeVote(), color:"#376FFF"}
-						  		];
-						  var fan = svg.select(".fans").selectAll("path.fan")
-						    .data(pie(dataset));
-
-						  fan.enter()
-						    .insert("path")
-						    .style("fill", function (d) {return d.data.color;})
-						    .attr("class", "fan");
-
-						  fan.transition().duration(1000)
-						  	.attrTween("d", function(d) {
-						  		this._current = this._current || d;
-						  		var interpolate = d3.interpolate(this._current, d);
-						  		this._current = interpolate(10);
-						  		return function(t) {
-						      	return arc(interpolate(t));
-						  		};
-						  	})
-
-						  fan.exit()
-						  	.remove();
-						}
-					</script>
+					
 					<!-- 小視窗 -->
 					<script>
 						$(document).ready(function(){
@@ -277,6 +269,19 @@ svg {
 								setTimeout("CheckMic()",1000);
 							}
 										CheckMic();*/
+					</script>
+
+					<script>
+						$(document).ready(function(){
+							$("#like").click(function(){
+								$("#like").fadeOut();
+								$("#dislike").fadeOut();
+							});
+							$("#dislike").click(function(){
+									$("#like").fadeOut();
+									$("#dislike").fadeOut();
+							});
+						});
 					</script>
 
 				<div class="queue">
