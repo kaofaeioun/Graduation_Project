c1cbfdb79a1325a15a96ae2de7f543dd84826344
Chen Yue-Yang <young5544112@gmail.com>
2016-11-23 22:56:23 +0800

    線上人數 lv2

--- PHP/Project/views/logoutconnect.php
+++ PHP/Project/views/logoutconnect.php
@@ -1,4 +1,12 @@
 <?php 
+	include("mysql_connect.php");
+	$user_now=$_COOKIE['account'];
+	$sql="UPDATE User SET User_Status='0' WHERE User_ID='$user_now'";
+	$result=mysqli_query($link,$sql);
+?>
+
+<?php 
 	setcookie('account','',time()-3600);
 	echo '<meta http-equiv=REFRESH CONTENT=0;url=login.php>';
 ?>
+
