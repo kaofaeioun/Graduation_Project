3ae3ca6ef2b2f8a69761776c76f8fa15db4c1a00
kaofaeioun <kaofaeioun@gmail.com>
2016-12-03 03:01:48 +0800

    傳

--- PHP/Project/chrome-extension-notifications-gh-pages/nojs/pubnub-setup.js
+++
@@ -1,5 +0,0 @@
-window.PUBNUB.setup = {
-    channel       : "Notification",
-    subscribe_key : "sub-c-d7a6417a-b717-11e6-b37b-02ee2ddab7fe",
-    publish_key   : "pub-c-2e3d03f8-d8b4-4ab4-99af-7eb241d7f26c"
-};
