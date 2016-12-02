4c40d41d22f7dee2ba62b05c93e0a2e691820e28
shaohonglee <d38372002@gmail.com>
2016-12-02 19:33:12 +0800

    刪投票紀錄

--- PHP/Project/views/cancelmic.php
+++ PHP/Project/views/cancelmic.php
@@ -5,4 +5,5 @@
 	$result2=mysqli_query($link,$sql2);
 	$sql3="UPDATE User SET `User_Status`='0' where `User_ID`='$singer'";
 	$result3=mysqli_query($link,$sql3);
+	$sql4="DELETE * FROM Vote where 1";
 ?>
